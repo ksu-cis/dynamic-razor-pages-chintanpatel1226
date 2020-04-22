f62b472a9b88e948850d081b277e28b3455e3668
Chintan Patel <chintanpatel1226@ksu.edu>
2020-04-22 11:01:20 -0500

    get help.

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,9 +9,22 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// The movies to display on the index page
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
 
+        /// <summary>
+        /// The current search terms
+        /// </summary>
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        /// Invokes every time the page is requested using a GET request. 
+        /// </summary>
         public void OnGet()
         {
+            String terms = Request.Query["SearchTerms"];
 
         }
     }

3ac4d81987b92d3fc3c25caee6a9f287dda62189
Chintan Patel <chintanpatel1226@ksu.edu>
2020-05-05 09:30:05 -0500

    done.

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -12,7 +12,7 @@ namespace Movies.Pages
         /// <summary>
         /// The filtered genres
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string[] Genres { get; set; }
 
 
@@ -25,56 +25,89 @@ namespace Movies.Pages
         /// <summary>
         /// Gets and sets the search terms
         /// </summary>
-        [BindProperty]
-        public string SearchTerms { get; set; } = " ";
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerms { get; set; }
 
         /// <summary>
         /// Gets and sets the MPAA rating filters
         /// </summary>
-        [BindProperty]
-        public string[] MPAARating { get; set; } = new string[0];
+        [BindProperty(SupportsGet = true)]
+        public string[] MPAARatings { get; set; }
 
 
         /// <summary>
         /// Gets and sets the IMDB minimium rating
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? IMDBMin { get; set; }
 
         /// <summary>
         /// Gets and sets the IMDB maximum rating
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? IMDBMax { get; set; }
 
         /// <summary>
         /// Gets and sets the Rotten Tomato minimium rating
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? RTMin { get; set; }
 
         /// <summary>
         /// Gets and sets the Rotten Tomato maximum rating
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? RTMax { get; set; }
 
         /// <summary>
         /// Does the response initialization for incoming GET requests
         /// </summary>
-        public void OnGet()
+        public void OnGet(double? IMDBMin, double? IMDBMax, double? RTMin, double? RTMax )
         {
             Movies = MovieDatabase.All;
-            
-        }
+            // Search movie titles for the SearchTerms
+            if (SearchTerms != null)
+            {
+                Movies = Movies.Where(movie => 
+                    movie.Title != null &&
+                    movie.Title.Contains(SearchTerms, StringComparison.InvariantCultureIgnoreCase));
+            }
 
-        public void OnPost()
-        {
-            Movies = MovieDatabase.Search(SearchTerms);
-            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARating);
-            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
-            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
-            Movies = MovieDatabase.FilterByRottenTomatoRatings(Movies, RTMin, RTMax);
+            // Filter by MPAA Rating 
+            if (MPAARatings != null && MPAARatings.Length != 0)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.MPAARating != null &&
+                    MPAARatings.Contains(movie.MPAARating));
+            }
+            
+            // Filter by Genre
+            if(Genres != null && Genres.Length != 0)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.MajorGenre != null &&
+                    Genres.Contains(movie.MajorGenre));
+            }
+
+            //Filter by IMDB Rating
+            if(IMDBMin != null || IMDBMax != null)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.IMDBRating != null &&
+                    (IMDBMin != null && IMDBMax == null && movie.IMDBRating >= IMDBMin) ||
+                    (IMDBMin == null && IMDBMax != null && movie.IMDBRating <= IMDBMax) ||
+                    (IMDBMin != null && IMDBMax != null && movie.IMDBRating >= IMDBMin && movie.IMDBRating <= IMDBMax));
+            }
+
+            //Filter by Rotten Tomatoes
+            if (RTMin != null || RTMax != null)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.RottenTomatoesRating != null &&
+                    (RTMin != null && RTMax == null && movie.RottenTomatoesRating >= RTMin) ||
+                    (RTMin == null && RTMax != null && movie.RottenTomatoesRating <= RTMax) ||
+                    (RTMin != null && RTMax != null && movie.RottenTomatoesRating >= RTMin && movie.RottenTomatoesRating <= RTMax));
+            }
         }
 
     }
